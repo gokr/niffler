1a623b87ca030be4277a75f5dc8d362a2ff8abcb
Göran Krampe <goran.krampe@gmail.com>
2025-09-10 23:44:04 +0200

    Added valgrind.sh script

---
+++ valgrind.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Running Valgrind..."
+echo "Building with debug symbols..."
+
+cd /home/gokr/tankfeud/niffler
+
+# Build with debug
+nim c --stackTrace:on --lineTrace:on --debugger:native --opt:none -d:debug --threads:on -d:ssl src/niffler.nim
+echo ""
+echo "/exit" | timeout 10s valgrind -s --tool=memcheck --leak-check=full --show-leak-kinds=all --track-origins=yes ./src/niffler 2>&1 | tee valgrind_output.log
+
+echo "Valgrind output saved to valgrind_output.log"
